d82b96b5da3ba31cf73b4b2837bd8aed44b3a2a3
Paul <paulll.lucas@gmail.com>
2017-10-18 20:02:52 -0700

    Boot -> Lein

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject sidewalk-clj "0.1.0-SNAPSHOP"
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/core.async "0.3.443"]
+                 [clj-opc "0.1.1"]
+                 [mount "0.1.11"]
+                 [com.rpl/specter "1.0.3"]])
